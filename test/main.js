b1570a8245d42fc8124bbb5671273d7a387ab4e3
Contra <contra@maricopa.edu>
2014-12-28 05:53:28 -0800

    speed up tests

--- test/main.js
+++ test/main.js
@@ -7,15 +7,19 @@ var mkdirp = require('mkdirp');
 
 require('mocha');
 
+// timeout is 100ms by default on gaze
+// so any file modifications are done on this interval
+var timeout = 100;
+
 describe('glob-watcher', function() {
   it('should return a valid file struct via EE', function(done) {
-    var expectedName = path.join(__dirname, "./fixtures/stuff/temp.coffee");
-    var fname = path.join(__dirname, "./fixtures/**/temp.coffee");
+    var expectedName = path.join(__dirname, './fixtures/stuff/temp.coffee');
+    var fname = path.join(__dirname, './fixtures/**/temp.coffee');
     mkdirp.sync(path.dirname(expectedName));
-    fs.writeFileSync(expectedName, "testing");
+    fs.writeFileSync(expectedName, 'testing');
 
     var watcher = watch(fname);
-    watcher.on('change', function(evt) {
+    watcher.once('change', function(evt) {
       should.exist(evt);
       should.exist(evt.path);
       should.exist(evt.type);
@@ -23,29 +27,29 @@ describe('glob-watcher', function() {
       evt.path.should.equal(expectedName);
       watcher.end();
     });
-    watcher.on('end', function(){
+    watcher.once('end', function(){
       rimraf.sync(expectedName);
       done();
     });
     setTimeout(function(){
-      fs.writeFileSync(expectedName, "test test");
-    }, 125);
+      fs.writeFileSync(expectedName, 'test test');
+    }, timeout);
   });
 
   it('should emit nomatch via EE', function(done) {
-    var fname = path.join(__dirname, "./doesnt_exist_lol/temp.coffee");
+    var fname = path.join(__dirname, './doesnt_exist_lol/temp.coffee');
 
     var watcher = watch(fname);
-    watcher.on('nomatch', function() {
+    watcher.once('nomatch', function() {
       done();
     });
   });
 
   it('should return a valid file struct via callback', function(done) {
-    var expectedName = path.join(__dirname, "./fixtures/stuff/test.coffee");
-    var fname = path.join(__dirname, "./fixtures/**/test.coffee");
+    var expectedName = path.join(__dirname, './fixtures/stuff/test.coffee');
+    var fname = path.join(__dirname, './fixtures/**/test.coffee');
     mkdirp.sync(path.dirname(expectedName));
-    fs.writeFileSync(expectedName, "testing");
+    fs.writeFileSync(expectedName, 'testing');
 
     var watcher = watch(fname, function(evt) {
       should.exist(evt);
@@ -56,32 +60,32 @@ describe('glob-watcher', function() {
       watcher.end();
     });
 
-    watcher.on('end', function(){
+    watcher.once('end', function(){
       rimraf.sync(expectedName);
       done();
     });
     setTimeout(function(){
-      fs.writeFileSync(expectedName, "test test");
-    }, 200);
+      fs.writeFileSync(expectedName, 'test test');
+    }, timeout);
   });
 
   it('should not return a non-matching file struct via callback', function(done) {
-    var expectedName = path.join(__dirname, "./fixtures/test123.coffee");
-    var fname = path.join(__dirname, "./fixtures/**/test.coffee");
+    var expectedName = path.join(__dirname, './fixtures/test123.coffee');
+    var fname = path.join(__dirname, './fixtures/**/test.coffee');
     mkdirp.sync(path.dirname(expectedName));
-    fs.writeFileSync(expectedName, "testing");
+    fs.writeFileSync(expectedName, 'testing');
 
     var watcher = watch(fname, function(evt) {
-      throw new Error("Should not have been called! "+evt.path);
+      throw new Error('Should not have been called! '+evt.path);
     });
 
     setTimeout(function(){
-      fs.writeFileSync(expectedName, "test test");
-    }, 200);
+      fs.writeFileSync(expectedName, 'test test');
+    }, timeout);
 
     setTimeout(function(){
       rimraf.sync(expectedName);
       done();
-    }, 1500);
+    }, timeout * 2);
   });
 });
