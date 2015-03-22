dbea1bff005bb9cc4bdf888f3f91058931d5bfeb
Blaine Bublitz <blaine@iceddev.com>
2015-03-22 14:12:21 -0700

    more test coverage and make the .add API more consistent

--- test/main.js
+++ test/main.js
@@ -65,6 +65,50 @@ describe('glob-watcher', function() {
     }, timeout);
   });
 
+  it('should call the callback registered with `add`', function(done) {
+    var expectedName = path.join(__dirname, './fixtures/stuff/test.coffee');
+    var fname = path.join(__dirname, './fixtures/**/test.coffee');
+    mkdirp.sync(path.dirname(expectedName));
+    fs.writeFileSync(expectedName, 'testing');
+
+    var watcher = watch();
+    watcher.add(fname, function() {
+      arguments.length.should.equal(0);
+      watcher.end();
+    });
+
+    watcher.once('end', function(){
+      rimraf.sync(expectedName);
+      done();
+    });
+    setTimeout(function(){
+      fs.writeFileSync(expectedName, 'test test');
+    }, timeout);
+  });
+
+  it('should not call a callback when filepath removed with `remove`', function(done) {
+    var expectedName = path.join(__dirname, './fixtures/stuff/test.coffee');
+    var fname = path.join(__dirname, './fixtures/**/test.coffee');
+    mkdirp.sync(path.dirname(expectedName));
+    fs.writeFileSync(expectedName, 'testing');
+
+    var watcher = watch();
+    watcher.add(fname, function() {
+      // if we get here, that's bad and should fail
+      true.should.equal(false);
+      watcher.end();
+    });
+    watcher.remove(expectedName);
+
+    setTimeout(function(){
+      fs.writeFileSync(expectedName, 'test test');
+    }, timeout);
+    setTimeout(function(){
+      rimraf.sync(expectedName);
+      done();
+    }, timeout * 3);
+  });
+
   it('should not return a non-matching file struct via callback', function(done) {
     var expectedName = path.join(__dirname, './fixtures/test123.coffee');
     var fname = path.join(__dirname, './fixtures/**/test.coffee');
