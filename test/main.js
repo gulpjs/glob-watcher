d7b2d6bdd863f88e2a7343fcbdb8afcac0e957f0
Blaine Bublitz <blaine.bublitz@gmail.com>
2015-03-22 13:31:02 -0700

    Breaking: Remove the change event object being passed to the callback & make `add` consistent

--- test/main.js
+++ test/main.js
@@ -45,18 +45,14 @@ describe('glob-watcher', function() {
     });
   });
 
-  it('should return a valid file struct via callback', function(done) {
+  it('should not pass any args via callback', function(done) {
     var expectedName = path.join(__dirname, './fixtures/stuff/test.coffee');
     var fname = path.join(__dirname, './fixtures/**/test.coffee');
     mkdirp.sync(path.dirname(expectedName));
     fs.writeFileSync(expectedName, 'testing');
 
-    var watcher = watch(fname, function(evt) {
-      should.exist(evt);
-      should.exist(evt.path);
-      should.exist(evt.type);
-      evt.type.should.equal('changed');
-      evt.path.should.equal(expectedName);
+    var watcher = watch(fname, function() {
+      arguments.length.should.equal(0);
       watcher.end();
     });
 
@@ -67,6 +63,50 @@ describe('glob-watcher', function() {
     setTimeout(function(){
       fs.writeFileSync(expectedName, 'test test');
     }, timeout);
+  });
+
+  it('should call the callback registered with `add`', function(done) {
+    var expectedName = path.join(__dirname, './fixtures/stuff/test.coffee');
+    var fname = path.join(__dirname, './fixtures/**/test.coffee');
+    mkdirp.sync(path.dirname(expectedName));
+    fs.writeFileSync(expectedName, 'testing');
+
+    var watcher = watch();
+    watcher.add(fname, function() {
+      arguments.length.should.equal(0);
+      watcher.end();
+    });
+
+    watcher.once('end', function(){
+      rimraf.sync(expectedName);
+      done();
+    });
+    setTimeout(function(){
+      fs.writeFileSync(expectedName, 'test test');
+    }, timeout);
+  });
+
+  it('should not call a callback when filepath removed with `remove`', function(done) {
+    var expectedName = path.join(__dirname, './fixtures/stuff/test.coffee');
+    var fname = path.join(__dirname, './fixtures/**/test.coffee');
+    mkdirp.sync(path.dirname(expectedName));
+    fs.writeFileSync(expectedName, 'testing');
+
+    var watcher = watch();
+    watcher.add(fname, function() {
+      // if we get here, that's bad and should fail
+      true.should.equal(false);
+      watcher.end();
+    });
+    watcher.remove(expectedName);
+
+    setTimeout(function(){
+      fs.writeFileSync(expectedName, 'test test');
+    }, timeout);
+    setTimeout(function(){
+      rimraf.sync(expectedName);
+      done();
+    }, timeout * 3);
   });
 
   it('should not return a non-matching file struct via callback', function(done) {
