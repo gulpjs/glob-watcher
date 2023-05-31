cbb4b18513ddaa3849692912c252c2e4eb33f0b4
Takayuki Sato <sttk.xslet@gmail.com>
2023-05-31 09:05:38 +0900

    chore!: Normalize repository, dropping node <10.13 support (#76)
    feat: Drop just-debounce from dependencies
    chore: Move args processing to separate file

---
+++ test/lib/debounce.test.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var expect = require('expect');
+var sinon = require('sinon');
+var debounce = require('../../lib/debounce');
+
+describe('lib/debounce', function() {
+
+  it('should call an original function with specified delay', function(done) {
+    expect.assertions(1);
+
+    var executed = false;
+    var fn = debounce(function() {
+      executed = true;
+    }, 10);
+
+    fn();
+
+    expect(executed).toBeFalsy();
+
+    setTimeout(function() {
+      expect(executed).toBeTruthy();
+      done();
+    }, 11);
+  });
+
+  it('should extend delay against multiple calls', function(done) {
+    expect.assertions(1);
+
+    var fn = debounce(function(a) {
+      expect(a).toBe(3);
+      done();
+    }, 50);
+
+    fn(1);
+    fn(2);
+
+    setTimeout(function() {
+      fn(3);
+    }, 3);
+  });
+
+  it('should extends delay if a preceding call doesn\'t run yet', function(done) {
+    expect.assertions(1);
+
+    var fn = debounce(function(a) {
+      expect(a).toBe(3);
+      done();
+    }, 10);
+
+    fn(1);
+
+    setTimeout(function() {
+      fn(2);
+
+      setTimeout(function() {
+        fn(3);
+      }, 5);
+    }, 5);
+  });
+
+  it('should run if a preceding call already run', function(done) {
+    expect.assertions(2);
+
+    var spy = sinon.spy(function(a) {
+      switch (spy.callCount) {
+        case 1:
+          expect(a).toBe(2);
+          break;
+        case 2:
+          expect(a).toBe(3);
+          done();
+          break;
+        default:
+          throw new Error();
+      }
+    });
+    var fn = debounce(spy, 6);
+
+    fn(1);
+
+    setTimeout(function() {
+      fn(2);
+
+      setTimeout(function() {
+        fn(3);
+      }, 10);
+    }, 3);
+  });
+});
---
+++ test/lib/normalize-args.test.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var expect = require('expect');
+var normalizeArgs = require('../../lib/normalize-args');
+
+describe('lib/normalize-args', function() {
+
+  it('should normalize glob to an array', function(done) {
+    var opts0 = {
+      delay: 500,
+      events: ['all'],
+      ignored: ['.*.txt'],
+      ignoreInitial: false,
+      queue: false,
+      persistent: true,
+    };
+    var cb0 = function() {};
+
+    normalizeArgs('*.txt', opts0, cb0, function(glob, opts, cb) {
+      expect(Array.isArray(glob)).toBeTruthy();
+      expect(opts).not.toBe(opts0);
+      expect(opts).toEqual(opts0);
+      expect(cb).toBe(cb0);
+      done();
+    });
+  });
+
+  it('should complement options with default options', function(done) {
+    var glob0 = ['*.txt'];
+    var cb0 = function() {};
+
+    normalizeArgs(glob0, {}, cb0, function(glob, opts, cb) {
+      expect(glob).not.toBe(glob0);
+      expect(glob).toEqual(glob0);
+      expect(opts).toEqual({
+        delay: 200,
+        events: ['add', 'change', 'unlink'],
+        ignored: [],
+        ignoreInitial: true,
+        queue: true,
+      });
+      expect(cb).toBe(cb0);
+      done();
+    });
+  });
+
+  it('should normalize options.events to an array', function(done) {
+    var glob0 = ['*.txt'];
+    var opts0 = {
+      events: 'all',
+    };
+    var cb0 = function() {};
+
+    normalizeArgs(glob0, opts0, cb0, function(glob, opts, cb) {
+      expect(glob).not.toBe(glob0);
+      expect(glob).toEqual(glob0);
+      expect(opts).toEqual({
+        delay: 200,
+        events: ['all'],
+        ignored: [],
+        ignoreInitial: true,
+        queue: true,
+      });
+      expect(cb).toBe(cb0);
+      done();
+    });
+  });
+
+  it('should change 2nd arg to cb if 2nd arg is a function', function(done) {
+    var glob0 = ['*.txt'];
+    var cb0 = function() {};
+    normalizeArgs(glob0, cb0, undefined, function(glob, opts, cb) {
+      expect(glob).not.toBe(glob0);
+      expect(glob).toEqual(glob0);
+      expect(opts).toEqual({
+        delay: 200,
+        events: ['add', 'change', 'unlink'],
+        ignored: [],
+        ignoreInitial: true,
+        queue: true,
+      });
+      expect(cb).toBe(cb0);
+      done();
+    });
+  });
+
+});
